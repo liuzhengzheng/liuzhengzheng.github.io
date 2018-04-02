193a73801220d8148f9187c02c200473f6d15128
liuzhengzheng <838789694@qq.com>
2018-04-02 13:44:18 +0800

    data资源抽离

---
+++ static/data.js
@@ -0,0 +1,147 @@
+export const Resources = [{
+  code: '1-1',
+  name: '前端资源',
+  data: {
+    '大前端': ['http://www.daqianduan.com/', '前端开发为主的综合性博客性媒体'],
+    '掘金': ['https://juejin.im/?utm_source=gold_browser_extension', '一个帮助开发者成长的社区'],
+    '技术胖': ['http://jspang.com/', '关注WEB前端技术，帮助web初学者学习'],
+    'CodePen': ['https://codepen.io/', '在线开发编辑器'],
+    'w3ctech': ['https://www.w3ctech.com/', 'web标准化交流会'],
+    'upyang': ['http://www.upyang.com/', '一个IT技术分享博客'],
+    'GithubPages': ['https://blog.csdn.net/yanzhenjie1003/article/details/51703370', 'GithubPages上搭建个人主页,亲测可用']
+  }
+},
+{
+  code: '1-2',
+  name: '前端框架',
+  data: {
+    'Vue.js': ['https://vuefe.cn/', 'Vue 2.0中文文档'],
+    'Vue学习路线（技术胖）': ['http://jspang.com/2018/01/21/vue-timeline/', '非常好的vue入门教程'],
+    'Vue踩坑点': ['https://blog.csdn.net/xidongdong1/article/details/78613654', 'Vue中一些需要注意的点'],
+    '深入理解Vue.js组件': ['https://blog.csdn.net/web_wusheng/article/details/70544280', 'CSDN']
+  }
+},
+{
+  code: '1-3',
+  name: '前端插件',
+  data: {
+    'select2中文': ['http://www.cnblogs.com/liuxiaobo93/p/5112993.html', '下拉选择框插件，详细参考官方网站']
+  }
+},
+{
+  code: '1-4',
+  name: 'Git源码',
+  data: {
+    'vue2-elm': ['https://github.com/bailicangdu/vue2-elm', '基于 vue2 + vuex 构建一个具有 45 个页面的大型单页面应用'],
+    'iView-Admin': ['https://github.com/iview/iview', 'iview发布的后台管理系统']
+  }
+},
+{
+  code: '1-5',
+  name: '前端工具',
+  data: {
+    '在线工具': ['http://tool.oschina.net/', '开源中国社区'],
+    '在线代码着色': ['http://tool.oschina.net/', '在线代码高亮着色'],
+    'Browsershot': ['http://browsershots.org/', '超多的浏览器兼容性在线测试，免去了在本机安装N多浏览器的'],
+    '二维码在线生成器': ['http://www.haorooms.com/tools/erweima/', '二维码在线生成器'],
+    'css在线选择器': ['http://www.haorooms.com/tools/css_selecter/', 'css在线选择器'],
+    'css3动画': ['http://www.haorooms.com/uploads/example/Animatecss/', '可以下载css各种动画代码'],
+    '小工具': ['http://www.haorooms.com/tools/index', '一些小工具']
+  }
+},
+{
+  code: '1-6',
+  name: '前端教程',
+  data: {
+    '慕课网': ['https://www.imooc.com/', '学习编程最简单的免费平台，慕课网提供了丰富的移动端开发、php开发、web前端、html5教程以及css3视频教程等课程资源。'],
+    '廖雪峰的官方网站': ['https://www.liaoxuefeng.com/', 'Js、Python和git教程']
+  }
+},
+{
+  code: '1-7',
+  name: '前端安全',
+  data: {
+    '浅谈CSRF攻击方式': ['https://www.cnblogs.com/hyddd/archive/2009/04/09/1432744.html', '简单明了解释了csrf攻击原理'],
+    'CSRF的攻击与防御': ['https://blog.csdn.net/stpeace/article/details/53512283', '写的很好（CSDN）'],
+    'XSS-Proxy': ['https://sourceforge.net/projects/xss-proxy/', '模拟xss攻击的工具'],
+    'BeEF': ['http://beefproject.com/', '模拟web攻击的平台']
+  }
+},
+{
+  code: '1-8',
+  name: '微信小程序',
+  data: {
+    '小程序文档': ['https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1474632113_xQVCl', '微信公众平台官方小程序文档'],
+    '小程序组件库小程序组件库xxxx': ['https://www.cnblogs.com/icyhoo/p/6282574.html', '微信小程序UI组件、开发框架、实用库'],
+    'WeUI': ['https://github.com/Tencent/weui-wxss', '小程序UI框架'],
+    '开发教程（简书）': ['https://www.jianshu.com/p/aaef5ceb3936', '微信小程序开发教程--从零开始（简书）']
+  }
+},
+{
+  code: '1-9',
+  name: '图标',
+  data: {
+    'Iconmonstr': ['https://iconmonstr.com/', '一套简笔Icon，包含各尺寸Icon上千个'],
+    'Icons DB': ['https://www.iconsdb.com/', '免费的图标下载网站，可选择多种尺寸和格式'],
+    'IOSIconGallery': ['http://www.iosicongallery.com/', '收录了苹果客户端的所有的ios产品的图标'],
+    'Iconfont': ['http://iconfont.cn/home/index', '阿里巴巴矢量图库'],
+    'Iconfever': ['https://iconfever.com/', '国外免费图标下载'],
+    'Favicon': ['https://www.favicon.cc/', '在线生成ico浏览器图标'],
+    'Dryicons': ['http://www.freeiconsdownload.com/', '按属性分类，国外免费图标下载'],
+    'FindIcons': ['https://findicons.com/', 'FindIcons图标搜索引擎'],
+    'Easy Icon': ['http://www.easyicon.net/', '图标搜索引擎，支持中文检索'],
+    '爱看图标': ['http://www.iconpng.com/', 'IconPng图标搜索引擎'],
+    'IconFinder': ['https://www.iconfinder.com/', 'iconfinder图标搜索引擎'],
+    'Icomoon': ['https://icomoon.io/', '强大的简约图标库'],
+    'Shapes4free': ['http://www.shapes4free.com/', '提供了免费的photoshop自定义形状、图标']
+  }
+},
+{
+  code: '1-10',
+  name: '图库',
+  data: {
+    '懒人图库': ['http://www.lanrentuku.com/', '矢量图，网页素材！'],
+    '昵图网': ['http://www.nipic.com/index.html', '昵图网，起步最早的图片素材网站！'],
+    '千图网': ['http://www.58pic.com/', '中国最大的免费图片网站！'],
+    '站酷': ['http://www.zcool.com.cn/', '设计师互动平台！图片资源也不少！'],
+    'UI中国': ['http://www.ui.cn/', '中国专业的界面设计平台！'],
+    '素材中国': ['http://www.sccnn.com/', '2006年创办的中文素材网站'],
+    'dribbble': ['https://dribbble.com/', '老外的设计网站！设计大牛比较多！']
+  }
+},
+{
+  code: '1-11',
+  name: '字体',
+  data: {
+    'Dafont': ['https://www.dafont.com/', '优秀英文字体精选，各个经典有特色'],
+    'DdFont': ['http://www.ddfont.com/', '直观的英文字体下载网站'],
+    'Fonts2u': ['http://zh.fonts2u.com/', '免费为Windows和Mac系统提供下载免费的字体'],
+    'Myfonts': ['http://www.myfonts.com/WhatTheFont/', '上传字体图片帮您找到英文字体安装文件'],
+    'ShowFont': ['http://www.showfont.net/', '英文字体下载网站，预览直观'],
+    'The Web Font': ['http://font-combinator.com/', '提供在线预览的文字组合排版工具'],
+    '求字体': ['http://www.qiuziti.com/', '上传字体图片帮您找到中文字体安装文件'],
+    '谷歌网络字体': ['http://www.google.com/webfonts', '个性字体不必用图片，用字符就可以实现']
+  }
+},
+{
+  code: '1-12',
+  name: '色彩',
+  data: {
+    '中国色彩大辞典': ['http://color.uisdc.com/', '中国/日本传统色彩命名，点击色彩可直接吸取色值'],
+    'Adobe配色工具': ['https://color.adobe.com/zh/', 'Adobe出品，网站设计师配色首选'],
+    'CssWinner网页色彩分类': ['http://www.csswinner.com/colorsearch/blue', 'CSS画廊，可根据右侧颜色块展现最流行的网页'],
+    'Coolors': ['https://coolors.co/browser/latest/1', '实用！成千上万的配色方案'],
+    '漂亮的渐变色': ['https://uigradients.com/#BlurryBeach', '流行的渐变！'],
+    'ColorHunter': ['http://www.colorhunter.com/', '上传照片提取配色方案'],
+    'colorfy': ['https://www.colorfyit.com/', '输入网站地址就能得到它的配色方案']
+  }
+},
+{
+  code: '1-13',
+  name: '设计',
+  data: {
+    '设计师网址导航': ['https://hao.uisdc.com/', '包括UI界面设计、字体、配色、交互等'],
+    '产品大牛': ['http://www.pmdaniu.com/explore', '很多大牛分享的优秀产品原型案例']
+  }
+}
+]
